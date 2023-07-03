bf935aaae6b67b3344daa230cfd174a2042fd26c
UriasDR <129330378+UriasDR@users.noreply.github.com>
2023-07-03 19:19:43 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ProcessamentoDeImagensWienerInverso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btLoad_A = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btFiltro = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.txVariance = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(314, 239);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(535, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(314, 239);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btLoad_A
+            // 
+            this.btLoad_A.BackColor = System.Drawing.SystemColors.Window;
+            this.btLoad_A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLoad_A.Location = new System.Drawing.Point(77, 299);
+            this.btLoad_A.Name = "btLoad_A";
+            this.btLoad_A.Size = new System.Drawing.Size(153, 31);
+            this.btLoad_A.TabIndex = 2;
+            this.btLoad_A.Text = "Carregar Imagem";
+            this.btLoad_A.UseVisualStyleBackColor = false;
+            this.btLoad_A.Click += new System.EventHandler(this.btLoad_A_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btFiltro
+            // 
+            this.btFiltro.BackColor = System.Drawing.SystemColors.Window;
+            this.btFiltro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btFiltro.Location = new System.Drawing.Point(357, 138);
+            this.btFiltro.Name = "btFiltro";
+            this.btFiltro.Size = new System.Drawing.Size(153, 31);
+            this.btFiltro.TabIndex = 5;
+            this.btFiltro.Text = "Filtro Inverso";
+            this.btFiltro.UseVisualStyleBackColor = false;
+            this.btFiltro.Click += new System.EventHandler(this.btFiltro_Click);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.BackColor = System.Drawing.SystemColors.Window;
+            this.btSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSalvar.Location = new System.Drawing.Point(632, 290);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(153, 31);
+            this.btSalvar.TabIndex = 6;
+            this.btSalvar.Text = "Salvar Imagem";
+            this.btSalvar.UseVisualStyleBackColor = false;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // txVariance
+            // 
+            this.txVariance.Location = new System.Drawing.Point(357, 102);
+            this.txVariance.Name = "txVariance";
+            this.txVariance.Size = new System.Drawing.Size(153, 20);
+            this.txVariance.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(861, 351);
+            this.Controls.Add(this.txVariance);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btFiltro);
+            this.Controls.Add(this.btLoad_A);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btLoad_A;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btFiltro;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.TextBox txVariance;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Accord.Imaging.Filters;
+using Accord.Math;
+
+namespace ProcessamentoDeImagensWienerInverso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap img1 = null;
+        byte[,] vImg1Gray;
+
+        private void btLoad_A_Click(object sender, EventArgs e)
+        {
+            
+
+            // Configurações iniciais da OpenFileDialogBox
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Armazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+                bool bLoadImgOK = false;
+                try
+                {
+                    img1 = new Bitmap(filePath);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    // Adiciona imagem na PictureBox
+                    pictureBox1.Image = img1;
+
+                    vImg1Gray = new byte[img1.Width, img1.Height];
+
+                    // Percorre todos os pixels da imagem...
+                    for (int i = 0; i < img1.Width; i++)
+                    {
+                        for (int j = 0; j < img1.Height; j++)
+                        {
+                            Color pixel = img1.GetPixel(i, j);
+
+                            // Para imagens em escala de cinza, extrair o valor do pixel com...
+                            byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            vImg1Gray[i, j] = pixelIntensity;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        private byte[,] ApplyWienerInverseFilter(byte[,] image, double noiseVariance)
+        {
+            int width = image.GetLength(0);
+            int height = image.GetLength(1);
+
+            // Realizar o processamento de filtragem inversa de Wiener
+            byte[,] filteredImage = new byte[width, height];
+
+            // Calcula o espectro de potência da imagem original
+            double[,] powerSpectrum = CalculatePowerSpectrum(image);
+
+            // Calcula o espectro de potência do ruído
+            double noisePower = noiseVariance * (width * height);
+
+            // Aplica o filtro de Wiener inverso
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    double wienerCoeff = powerSpectrum[i, j] / (powerSpectrum[i, j] + noisePower);
+                    double intensity = image[i, j] / wienerCoeff;
+                    filteredImage[i, j] = (byte)Math.Round(intensity);
+                }
+            }
+
+            return filteredImage;
+        }
+
+        private double[,] CalculatePowerSpectrum(byte[,] image)
+        {
+            int width = image.GetLength(0);
+            int height = image.GetLength(1);
+
+            // Nova matriz para calcular o espectro da potência
+            double[,] powerSpectrum = new double[width, height];
+
+            // Calcula o espectro de potência como o quadrado do valor de intensidade
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    double intensity = image[i, j];
+                    powerSpectrum[i, j] = intensity * intensity;
+                }
+            }
+
+            return powerSpectrum;
+        }
+
+        private Bitmap ConvertToBitmap(byte[,] image)
+        {
+            int width = image.GetLength(0);
+            int height = image.GetLength(1);
+
+            Bitmap bitmap = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    byte pixelValue = image[i, j];
+                    Color pixelColor = Color.FromArgb(pixelValue, pixelValue, pixelValue);
+                    bitmap.SetPixel(i, j, pixelColor);
+                }
+            }
+
+            return bitmap;
+        }
+
+        private void btFiltro_Click(object sender, EventArgs e)
+        {
+            if (txVariance.Text.Length < 1) txVariance.Text = "0";
+            double iVariance = Double.Parse(txVariance.Text);
+
+
+            // Aplicar o filtro de Wiener inverso na imagem em escala de cinza
+            byte[,] filteredImage = ApplyWienerInverseFilter(vImg1Gray, iVariance);
+
+            // Exibir a imagem filtrada na PictureBox
+            Bitmap filteredBitmap = ConvertToBitmap(filteredImage);
+            pictureBox2.Image = filteredBitmap;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            Bitmap imagem = (Bitmap)pictureBox2.Image;
+
+            if (imagem != null)
+            {
+                // Define o caminho do arquivo onde a imagem será salva
+                string caminhoArquivo = "C:\\Matlab\\Imagem.Bmp";
+                // Salva a imagem no arquivo
+                imagem.Save(caminhoArquivo, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                // Exibe uma mensagem de sucesso
+                MessageBox.Show("Imagem salva com sucesso!");
+            }
+            else
+            {
+                // Exibe uma mensagem de erro se a imagem não estiver definida
+                MessageBox.Show("A imagem não está definida!");
+            }
+        }
+    }
+}
